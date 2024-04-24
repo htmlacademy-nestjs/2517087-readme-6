8b4e743c94c295791aa43d275a1b1eb73fb6ce69
BulleT <BulleTpac@mail.ru>
2024-04-24 16:54:41 +0300

    chore(lib): add new interfaces and clean lib core

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/core';
+export { Entity } from './lib/base/entity';
+export { UserRole } from './lib/types/user-role.enum';
+export { User } from './lib/types/user.interface';
+export { AuthUser } from './lib/types/auth-user.interface';
