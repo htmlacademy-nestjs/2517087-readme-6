6f51b04e1edd1bd786844d2a833b29c7b7b20907
BulleT <BulleTpac@mail.ru>
2024-04-25 23:06:38 +0300

    feat(libs): add `register` in `AuthenticationService`, add `findByEmail` in `user-info.rep`

--- project/libs/user/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/user/authentication/src/authentication-module/authentication.service.ts
@@ -1,10 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import dayjs from "dayjs";
+import {ConflictException, Injectable} from '@nestjs/common';
 
-import { UserInfoRepository } from '@project/user-info';
+import {UserInfoEntity, UserInfoRepository} from '@project/user-info';
+import {CreateUserDto} from "../dto/create-user.dto";
+import {UserRole} from "@project/shared/core";
+
+import {AUTH_USER_EXISTS} from "./authentication.constant";
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     private readonly userInfoRepository: UserInfoRepository
   ) {}
+
+  public async register(dto: CreateUserDto) {
+    const {email, firstname, lastname, password, dateBirth} = dto;
+
+    const blogUser = {
+      email, firstname, lastname, role: UserRole.User,
+      avatar: '', dateOfBirth: dayjs(dateBirth).toDate(),
+      passwordHash: ''
+    };
+
+    const existUser = await this.userInfoRepository
+      .findByEmail(email);
+
+    if (existUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new UserInfoEntity(blogUser)
+      .setPassword(password)
+
+    return this.userInfoRepository
+      .save(userEntity);
+  }
 }
--- project/libs/user/user-info/src/user-info-module/user-info.repository.ts
+++ project/libs/user/user-info/src/user-info-module/user-info.repository.ts
@@ -9,4 +9,15 @@ export class UserInfoRepository extends BaseMemoryRepository<UserInfoEntity> {
   constructor(entityFactory: UserInfoFactory) {
     super(entityFactory);
   }
+
+  public async findByEmail(email: string): Promise<UserInfoEntity | null> {
+    const entities = Array.from(this.entities.values());
+    const user = entities.find((entity) => entity.email === email);
+
+    if (! user) {
+      return null;
+    }
+
+    return this.entityFactory.create(user);
+  }
 }
