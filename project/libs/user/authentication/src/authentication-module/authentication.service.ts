3d259abff02172b5ff9db9d9b2b68e8cbec733c4
BulleT <BulleTpac@mail.ru>
2024-04-25 23:10:01 +0300

    feat(auth): add `verifyUser` in auth service, add new const

--- project/libs/user/authentication/src/authentication-module/authentication.service.ts
+++ project/libs/user/authentication/src/authentication-module/authentication.service.ts
@@ -1,11 +1,12 @@
 import dayjs from "dayjs";
-import {ConflictException, Injectable} from '@nestjs/common';
+import {ConflictException, Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
 
 import {UserInfoEntity, UserInfoRepository} from '@project/user-info';
 import {CreateUserDto} from "../dto/create-user.dto";
 import {UserRole} from "@project/shared/core";
 
-import {AUTH_USER_EXISTS} from "./authentication.constant";
+import {AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG} from "./authentication.constant";
+import {LoginUserDto} from "../dto/login-user.dto";
 
 @Injectable()
 export class AuthenticationService {
@@ -34,5 +35,30 @@ export class AuthenticationService {
 
     return this.userInfoRepository
       .save(userEntity);
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const {email, password} = dto;
+    const existUser = await this.userInfoRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    if (!await existUser.comparePassword(password)) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return existUser;
+  }
+
+  public async getUser(id: string) {
+    const user = await this.userInfoRepository.findById(id);
+
+    if (! user) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    return user;
   }
 }
