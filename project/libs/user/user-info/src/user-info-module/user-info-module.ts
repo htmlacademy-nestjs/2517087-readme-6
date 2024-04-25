436ee02bc04ab11ec6ae777edbbf5f2e842ebd59
BulleT <BulleTpac@mail.ru>
2024-04-25 22:01:54 +0300

    feat(user-info): Injectable & Providers `UserInfoFactory & UserInfoRepository`

--- project/libs/user/user-info/src/user-info-module/user-info-module.ts
+++ project/libs/user/user-info/src/user-info-module/user-info-module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { UserInfoRepository } from './user-info.repository';
+import { UserInfoFactory } from './user-info.factory';
+
+@Module({
+  providers: [UserInfoRepository, UserInfoFactory],
+  exports: [UserInfoRepository],
+})
 export class UserInfoModule {}
--- project/libs/user/user-info/src/user-info-module/user-info.repository.ts
+++ project/libs/user/user-info/src/user-info-module/user-info.repository.ts
@@ -1,8 +1,10 @@
-import { BaseMemoryRepository } from '@project/data-access';
+import {Injectable} from "@nestjs/common";
 
+import { BaseMemoryRepository } from '@project/data-access';
 import { UserInfoEntity } from './user-info.entity';
 import { UserInfoFactory } from './user-info.factory';
 
+@Injectable()
 export class UserInfoRepository extends BaseMemoryRepository<UserInfoEntity> {
   constructor(entityFactory: UserInfoFactory) {
     super(entityFactory);
