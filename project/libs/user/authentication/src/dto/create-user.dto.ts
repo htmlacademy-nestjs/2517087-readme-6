f9825045be97d599c57ca26d822905ff8a8c88b2
BulleT <BulleTpac@mail.ru>
2024-04-26 13:29:36 +0300

    chore(libs): add const `AuthResponseMessage`, add API decorators, install package `@nestjs/config`

--- project/libs/user/authentication/src/dto/create-user.dto.ts
+++ project/libs/user/authentication/src/dto/create-user.dto.ts
@@ -1,7 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User unique address',
+    example: 'user@user.ru'
+  })
   public email: string;
+
+  @ApiProperty({
+    description: 'User birth date',
+    example: '1990-02-22',
+  })
   public dateBirth: string;
+
+  @ApiProperty({
+    description: 'User first name',
+    example: 'Alex',
+  })
   public firstname: string;
+
+  @ApiProperty({
+    description: 'User last name',
+    example: 'Ivanov'
+  })
   public lastname: string;
+
+  @ApiProperty({
+    description: 'User Password',
+    example: 'Querty123'
+  })
   public password: string;
 }
