e066f04c9891c2934125680293270232bbe63af2
BulleT <BulleTpac@mail.ru>
2024-04-26 09:44:43 +0300

    feat(mainApp): install `swagger` & add config in main, fix comments :: add const port & glob prefix

--- project/apps/user/src/main.ts
+++ project/apps/user/src/main.ts
@@ -1,20 +1,27 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+
+const GLOBAL_PREFIX = 'api';
+const DEFAULT_PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+
+  const config = new DocumentBuilder()
+    .setTitle('User Application')
+    .setDescription('User application API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || DEFAULT_PORT;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`);
 }
 
 bootstrap();
