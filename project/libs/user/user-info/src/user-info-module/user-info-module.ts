081623040b9a76dc8ce27931a435a047044fb7ea
BulleT <BulleTpac@mail.ru>
2024-04-26 20:43:25 +0300

    feat(userInfoModule): add `UserInfoModule` in userInfoModule-imports,update `BlogUserRepository` use `BaseMongoRepository`

--- project/libs/user/user-info/src/user-info-module/user-info-module.ts
+++ project/libs/user/user-info/src/user-info-module/user-info-module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import {MongooseModule} from "@nestjs/mongoose";
 
 import { UserInfoRepository } from './user-info.repository';
 import { UserInfoFactory } from './user-info.factory';
+import { UserInfoModel, UserInfoSchema } from "./user-info.model";
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: UserInfoModel.name, schema: UserInfoSchema }
+  ])],
   providers: [UserInfoRepository, UserInfoFactory],
   exports: [UserInfoRepository],
 })
--- project/libs/user/user-info/src/user-info-module/user-info.repository.ts
+++ project/libs/user/user-info/src/user-info-module/user-info.repository.ts
@@ -1,23 +1,25 @@
-import {Injectable} from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
 
-import { BaseMemoryRepository } from '@project/data-access';
+import { BaseMongoRepository } from '@project/data-access';
+
 import { UserInfoEntity } from './user-info.entity';
 import { UserInfoFactory } from './user-info.factory';
+import { UserInfoModel } from './user-info.model';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class UserInfoRepository extends BaseMemoryRepository<UserInfoEntity> {
-  constructor(entityFactory: UserInfoFactory) {
-    super(entityFactory);
+export class UserInfoRepository extends BaseMongoRepository<UserInfoEntity, UserInfoModel> {
+  constructor(
+    entityFactory: UserInfoFactory,
+    @InjectModel(UserInfoModel.name)
+    userInfoModel: Model<UserInfoModel>
+  ) {
+    super(entityFactory, userInfoModel);
   }
 
   public async findByEmail(email: string): Promise<UserInfoEntity | null> {
-    const entities = Array.from(this.entities.values());
-    const user = entities.find((entity) => entity.email === email);
-
-    if (! user) {
-      return null;
-    }
-
-    return this.entityFactory.create(user);
+    const document = await this.model.findOne({ email }).exec();
+    return this.createEntityFromDocument(document);
   }
 }
