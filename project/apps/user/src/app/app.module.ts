e68cb8df5c4fd5a58c667d163263fe302207233d
BulleT <BulleTpac@mail.ru>
2024-04-26 16:04:38 +0300

    feat(config): add `MongoConfig`, import `UserConfigModule` in `appModule`, add const `configService` in `main.ts`

--- project/apps/user/src/app/app.module.ts
+++ project/apps/user/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserInfoModule } from "@project/user-info";
 import { AuthenticationModule } from "@project/authentication";
+import {UserConfigModule} from "@project/user-config";
 
 @Module({
-  imports: [ UserInfoModule, AuthenticationModule ],
+  imports: [ UserInfoModule, AuthenticationModule, UserConfigModule ],
   controllers: [],
   providers: [],
 })
--- project/libs/user/config/src/user-config.module.ts
+++ project/libs/user/config/src/user-config.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import applicationConfig from './configurations/app.config';
+import mongoConfig from "./configurations/mongo.config";
 
 const ENV_USERS_FILE_PATH = 'apps/user/user.env';
 
@@ -10,8 +11,7 @@ const ENV_USERS_FILE_PATH = 'apps/user/user.env';
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      // TODO: Передать список конфигураций для загрузки
-      load: [applicationConfig],
+      load: [applicationConfig, mongoConfig],
       envFilePath: ENV_USERS_FILE_PATH
     }),
   ]
