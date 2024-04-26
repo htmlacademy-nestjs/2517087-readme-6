3f08f0a314ed1d2128e6edcf32a8d857b5f8aa01
BulleT <BulleTpac@mail.ru>
2024-04-26 14:26:09 +0300

    feat(config): init package `joi`, add new `applicationConfig` in load `forRoot` - userConfigModule

--- project/libs/user/config/src/user-config.module.ts
+++ project/libs/user/config/src/user-config.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import applicationConfig from './configurations/app.config';
+
 const ENV_USERS_FILE_PATH = 'apps/user/user.env';
 
 @Module({
@@ -9,7 +11,7 @@ const ENV_USERS_FILE_PATH = 'apps/user/user.env';
       isGlobal: true,
       cache: true,
       // TODO: Передать список конфигураций для загрузки
-      load: [],
+      load: [applicationConfig],
       envFilePath: ENV_USERS_FILE_PATH
     }),
   ]
