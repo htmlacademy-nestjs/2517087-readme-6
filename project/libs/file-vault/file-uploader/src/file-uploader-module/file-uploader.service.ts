1e8bb2933a308f826ce375b310fc04f19a3eea94
BulleT <BulleTpac@mail.ru>
2024-05-11 00:47:50 +0300

    feat(fileUploader): add `serveStaticOptions` for `ServeStaticModule`, add `year&month` in `getUploadDirectoryPath`, add uniq name for file

--- project/libs/file-vault/file-uploader/src/file-uploader-module/file-uploader.service.ts
+++ project/libs/file-vault/file-uploader/src/file-uploader-module/file-uploader.service.ts
@@ -2,8 +2,11 @@ import 'multer';
 import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { ensureDir } from 'fs-extra';
+import { extension } from 'mime-types';
+import dayjs from 'dayjs';
 import { writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
+import { randomUUID } from 'node:crypto';
 
 import { FileVaultConfig } from '@project/file-vault-config';
 
@@ -17,17 +20,21 @@ export class FileUploaderService {
   ) {}
 
   private getUploadDirectoryPath(): string {
-    return this.config.uploadDirectory;
+    const [year, month] = dayjs().format('YYYY MM').split(' ');
+    return join(this.config.uploadDirectory, year, month);
   }
 
   private getDestinationFilePath(filename: string): string {
-    return join(this.getUploadDirectoryPath(), filename)
+    return join(this.getUploadDirectoryPath(), filename);
   }
 
   public async saveFile(file: Express.Multer.File): Promise<string> {
     try {
       const uploadDirectoryPath = this.getUploadDirectoryPath();
-      const destinationFile = this.getDestinationFilePath(file.originalname);
+      const filename = randomUUID();
+      const fileExtension = extension(file.mimetype);
+
+      const destinationFile = this.getDestinationFilePath(`${filename}.${fileExtension}`);
 
       await ensureDir(uploadDirectoryPath);
       await writeFile(destinationFile, file.buffer);
