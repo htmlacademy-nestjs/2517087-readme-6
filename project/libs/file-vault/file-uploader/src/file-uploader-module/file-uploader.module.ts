63f02a2bd7d9e6c1d4491529772a34c2506b8180
BulleT <BulleTpac@mail.ru>
2024-05-10 23:33:16 +0300

    feat(fileUploader): init `@nestjs/serve-static`, import `ServeStaticModule` in `FileUploaderModule`, replace path module

---
+++ project/libs/file-vault/file-uploader/src/file-uploader-module/file-uploader.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigService } from '@nestjs/config';
+
+import { FileUploaderService } from './file-uploader.service';
+import { FileUploaderController } from './file-uploader.controller';
+
+const SERVE_ROOT = '/static';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const rootPath = configService.get<string>('application.uploadDirectory');
+        return [{
+          rootPath,
+          serveRoot: SERVE_ROOT,
+        }]
+      }
+    })
+  ],
+  providers: [FileUploaderService],
+  controllers: [FileUploaderController],
+})
+export class FileUploaderModule {}
