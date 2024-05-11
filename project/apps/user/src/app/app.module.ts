751f2e2463ba9904a4deb6fdfffa8faab5555ff7
BulleT <BulleTpac@mail.ru>
2024-05-11 22:46:09 +0300

    feat(rabbitMQ): add `notify` in `users`, add new lib `user-notify`

--- project/apps/user/src/app/app.module.ts
+++ project/apps/user/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserInfoModule } from "@project/user-info";
 import { AuthenticationModule } from "@project/authentication";
 import { UserConfigModule, getMongooseOptions } from "@project/user-config";
+import { NotifyModule } from '@project/user-notify';
 
 @Module({
   imports: [ UserInfoModule, AuthenticationModule, UserConfigModule, MongooseModule.forRootAsync(
       getMongooseOptions()
-    )
+    ),
+    NotifyModule,
   ],
   controllers: [],
   providers: [],
